73bf5577c7bf64c46278ae9c44f9581fa50a06a2
Vishwajeet KUmbhar <vishwajeetkumbhar97@gmail.com>
2023-12-07 12:39:54 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const app = express();
+const mongoose = require("mongoose")
+require("./db/conn");
+const users = require("./model/userSchema")
+const cors = require("cors")
+const port = process.env.PORT || 8005
+const router = require("./router/router")
+const cookieParser = require("cookie-parser")
+require("dotenv").config()
+const authenticate = require("./middleware/authenticat")
+require('dotenv').config();
+app.use(cors());
+app.use(cookieParser(""))
+app.use(express.json());
+app.use(router);
+
+app.listen(port, () => {
+    console.log("server is start on Port 8005")
+})
+
